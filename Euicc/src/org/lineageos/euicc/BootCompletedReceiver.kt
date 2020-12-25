2ea8da8123ca71cbf19bd7641da3655725659a5f
Peter Cai <peter@typeblog.net>
2020-12-25 19:00:13 +0800

    Euicc: Disable EuiccGoogle when GMS and GSF are not installed
    * This avoids EuiccGoogle / GoogleCarrierServices crashing, taking the
      whole telephony stack with them when no GMS or GSF is installed.
    * This approach is no-op when GAPPS add-on is flashed before the first
      boot, and thus should not cause issues with eSIM when GAPPS is
      flashed.
    * With microG, this also works after installing the microG GMS & GSF
      packages and rebooting, although manual activation of the eSIM is
      needed via Settings - Network.
    * The only downside is that users will still see one or two crashes
      during the first boot because our application only starts after the
      device is booted.
    
    Change-Id: Ice053b623d8f4a8e3783d82ffe0dc67f2cba558e

---
+++ Euicc/src/org/lineageos/euicc/BootCompletedReceiver.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.euicc
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received boot completed intent")
+        EuiccDisabler.enableOrDisableEuicc(context)
+    }
+
+    companion object {
+        private const val TAG = "XiaomiEuiccBootReceiver"
+    }
+}
---
+++ Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.euicc
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
+import android.util.Log
+
+object EuiccDisabler {
+    private const val TAG = "XiaomiEuiccDisabler"
+
+    private val EUICC_DEPENDENCIES = listOf(
+        "com.google.android.gms",
+        "com.google.android.gsf",
+    )
+
+    private val EUICC_PACKAGES = listOf(
+        "com.google.android.euicc",
+        "com.google.euiccpixel",
+        "com.google.android.ims",
+    )
+
+    private fun isInstalledAndEnabled(pm: PackageManager, pkgName: String) = runCatching {
+        val info = pm.getPackageInfo(pkgName, PackageInfoFlags.of(0))
+        Log.d(TAG, "package $pkgName installed, enabled = ${info.applicationInfo.enabled}")
+        info.applicationInfo.enabled
+    }.getOrDefault(false)
+
+    fun enableOrDisableEuicc(context: Context) {
+        val pm = context.packageManager
+        val disable = EUICC_DEPENDENCIES.any { !isInstalledAndEnabled(pm, it) }
+        val flag = if (disable) {
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+        } else {
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+        }
+
+        for (pkg in EUICC_PACKAGES) {
+            pm.setApplicationEnabledSetting(pkg, flag, 0)
+        }
+    }
+}
