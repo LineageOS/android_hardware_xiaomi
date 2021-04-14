9fd3926d5f3f77e17246aaf000e9679fffc9ee3e
Wei Wang <wvw@google.com>
2021-04-14 10:21:50 -0700

    power: reduce log spam and refine trace title
    Add bunch of TODO for team as well.
    
    Test: build
    Bug: 177492680
    Bug: 185368789
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: Ic1d5ecea10a60b23343866cd62519fda37bd6ec5

--- aidl/power-libperfmgr/Power.h
+++ aidl/power-libperfmgr/Power.h
@@ -56,6 +56,7 @@ class Power : public ::aidl::android::hardware::power::BnPower {
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mSustainedPerfModeOn;
     const int64_t mAdpfRate;
+    // TODO(jimmyshiu@): hold weak_ptr of PowerHintSession
 };
 
 }  // namespace pixel
