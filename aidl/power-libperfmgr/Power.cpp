170f14a1cc27033413ee04b972f7cf754296b670
Volodymyr Zhdanov <wight554@gmail.com>
2021-06-23 08:00:18 +0000

    power-libperfmgr: move dt2w mode on top of setMode switch
    * fixes dt2w after reboot
    
    Change-Id: I6151c91f89aeb4599b65b210773667afa91d0b3f

--- aidl/power-libperfmgr/Power.cpp
+++ aidl/power-libperfmgr/Power.cpp
@@ -87,6 +87,12 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     }
 #endif
     switch (type) {
+#ifdef TAP_TO_WAKE_NODE
+        case Mode::DOUBLE_TAP_TO_WAKE:        
+	               ::android::base::WriteStringToFile(enabled ? "1" : "0",
+                               TAP_TO_WAKE_NODE, true);
+            break;
+#endif
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
                 mHintManager->DoHint("SUSTAINED_PERFORMANCE");
@@ -98,14 +104,10 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
                 break;
             }
             [[fallthrough]];
+#ifndef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
-#ifdef TAP_TO_WAKE_NODE
-	               ::android::base::WriteStringToFile(enabled ? "1" : "0",
-                               TAP_TO_WAKE_NODE, true);
-            break;
-#else
             [[fallthrough]];
-#endif
+#endif        
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
         case Mode::EXPENSIVE_RENDERING:
