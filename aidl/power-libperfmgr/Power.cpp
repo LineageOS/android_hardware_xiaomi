d865b86327017fcc5be6658943f38163eb5e2c1f
Stefanie Kondik <shade@chemlab.org>
2021-06-23 07:49:15 +0000

    power-libperfmgr: Add support for tap-to-wake feature control
    [wight554] Enable DOUBLE_TAP_TO_WAKE mode explicitly and move dt2w mode on top of setMode switch: when LAUNCH hint is received, if mSustainedPerfModeOn is not true, it will not break, leading to the case switch going through all the next labels without interrupting until the end, also hitting DT2W one, leading to DT2W always toggling when you launch an app
    [SebaUbuntu] Don't break devices using hint manager for DT2W toggle
    
    Change-Id: Ib6e1e010834b1b16668d52db79b26804fe0e1671

--- aidl/power-libperfmgr/Power.cpp
+++ aidl/power-libperfmgr/Power.cpp
@@ -87,6 +87,11 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     }
 #endif
     switch (type) {
+#ifdef TAP_TO_WAKE_NODE
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
+            break;
+#endif
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
                 mHintManager->DoHint("SUSTAINED_PERFORMANCE");
@@ -98,8 +103,10 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
                 break;
             }
             [[fallthrough]];
+#ifndef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
             [[fallthrough]];
+#endif
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
         case Mode::EXPENSIVE_RENDERING:
@@ -132,6 +139,11 @@ ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
 #endif
 
     bool supported = mHintManager->IsHintSupported(toString(type));
+#ifdef TAP_TO_WAKE_NODE
+    if (type == Mode::DOUBLE_TAP_TO_WAKE) {
+        supported = true;
+    }
+#endif
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
     *_aidl_return = supported;
     return ndk::ScopedAStatus::ok();
