a58f2d3fd443c80f059ce8cec04ddcc3810c5db6
LuK1337 <priv.luk@gmail.com>
2021-09-16 13:55:07 +0200

    hidl: Implement vendor.lineage.powershare@1.0 HAL for Xiaomi
     * This feature is present in Xiaomi Mi 10/Pro/Ultra and for the Xiaomi
       family of SM8350, LAHAINA devices.
    
    Change-Id: Icabcee87980611d97a837f86a52d94033de337ef

---
+++ hidl/powershare/PowerShare.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "PowerShareService"
+
+#include "PowerShare.h"
+#include <hidl/HidlTransportSupport.h>
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace powershare {
+namespace V1_0 {
+namespace implementation {
+
+#define WIRELESS_TX_ENABLE_PATH "/proc/wireless/enable_tx"
+
+/*
+ * Write value to path and close file.
+ */
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+Return<bool> PowerShare::isEnabled() {
+    return get<std::string>(WIRELESS_TX_ENABLE_PATH, "disable") != "disable";
+}
+
+Return<bool> PowerShare::setEnabled(bool enable) {
+    set(WIRELESS_TX_ENABLE_PATH, enable ? 1 : 0);
+
+    return isEnabled();
+}
+
+Return<uint32_t> PowerShare::getMinBattery() {
+    return 0;
+}
+
+Return<uint32_t> PowerShare::setMinBattery(uint32_t) {
+    return getMinBattery();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace powershare
+}  // namespace lineage
+}  // namespace vendor
