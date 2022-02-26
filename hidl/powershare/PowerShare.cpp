ad2412e8f2059f362c339a16a7d058a64696c661
LuK1337 <priv.luk@gmail.com>
2022-02-26 21:01:09 +0100

    powershare: Change fallback enable value to 0
    Some variants don't support wireless charging, thus this file is
    missing. Ideally we wouldn't run powershare at all on them, but this is
    still an improvement.
    
    Change-Id: Ie58aaac2a73055d8d2cfe7a73b9b479493eb2bcf

--- hidl/powershare/PowerShare.cpp
+++ hidl/powershare/PowerShare.cpp
@@ -35,7 +35,7 @@ static T get(const std::string& path, const T& def) {
 }
 
 Return<bool> PowerShare::isEnabled() {
-    const auto value = get<std::string>(WIRELESS_TX_ENABLE_PATH, "");
+    const auto value = get<std::string>(WIRELESS_TX_ENABLE_PATH, "0");
     return !(value == "disable" || value == "0");
 }
 
