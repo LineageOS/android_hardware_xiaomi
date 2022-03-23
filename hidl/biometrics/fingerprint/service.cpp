4d011c210764ed0c28e261d25bd0a7977332d4e5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-23 15:55:44 +0100

    hidl: biometrics: fingerprint: Wire up Xiaomi FingerprintExtension
    Co-authored-by: C457 <android.c357@gmail.com>
    Change-Id: Ic94f7b3fd28f7db81695e47f54ce6fa89248bde9

--- hidl/biometrics/fingerprint/service.cpp
+++ hidl/biometrics/fingerprint/service.cpp
@@ -12,6 +12,7 @@
 #include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
 #include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
+#include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
 #include "BiometricsFingerprint.h"
 
 using android::sp;
@@ -19,9 +20,12 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
 using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
+using vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
 
 int main() {
-    android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
+    android::sp<IBiometricsFingerprint> bio =
+            BiometricsFingerprint::getInstance<IBiometricsFingerprint>();
+    android::sp<IXiaomiFingerprint> xfe = BiometricsFingerprint::getInstance<IXiaomiFingerprint>();
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
@@ -31,6 +35,14 @@ int main() {
         }
     } else {
         LOG(ERROR) << "Can't create instance of BiometricsFingerprint, nullptr";
+    }
+
+    if (xfe != nullptr) {
+        if (::android::OK != xfe->registerAsService()) {
+            return 1;
+        }
+    } else {
+        ALOGE("Can't create instance of XiaomiFingerprint, nullptr");
     }
 
     joinRpcThreadpool();
