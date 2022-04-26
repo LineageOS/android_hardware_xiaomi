939261d47a96ad7e1f0c4fecd8167cc8d08476e7
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-26 23:43:51 +0000

    hidl: Implement vendor.lineage.touch@1.0 HAL for Xiaomi
    * This can be used by any Xiaomi device that has a touch display that supports multiple sample rates and Xiaomi touch.
    * Most newer Xiaomi phones with high screen refresh rates fit this category:
    * e.g. Mi 10/Pro/Ultra/T/T Pro, Xiaomi SM8350 platform phones & Xiaomi SM6375 phones
    
    Change-Id: Iaf956420157bbc978690fac9053d507ccaea847a

---
+++ hidl/touch/HighTouchPollingRate.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "HighTouchPollingRateService"
+
+#include "HighTouchPollingRate.h"
+
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+#define HIGH_TOUCH_POLLING_PATH = "/sys/devices/virtual/touch/touch_dev/bump_sample_rate"
+
+Return<bool> HighTouchPollingRate::isEnabled() {
+    std::ifstream file(HIGH_TOUCH_POLLING_PATH);
+    int enabled;
+    file >> enabled;
+
+    return enabled == 1;
+}
+
+Return<bool> HighTouchPollingRate::setEnabled(bool enabled) {
+    std::ofstream file(HIGH_TOUCH_POLLING_PATH);
+    file << (enabled ? "1" : "0");
+    return !file.fail();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
