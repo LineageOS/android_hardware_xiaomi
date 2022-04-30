ab8039ba8a0557b69f0f00502c61838e5290f046
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-30 23:06:03 +0200

    hidl: biometrics: fingerprint: Rebrand to Xiaomi
    Change-Id: Icd5dfab05ca571ef36e8b2b1ce1bad9239877282

--- hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
+++ hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.xiaomi"
 
 #include <hardware/hw_auth_token.h>
 
