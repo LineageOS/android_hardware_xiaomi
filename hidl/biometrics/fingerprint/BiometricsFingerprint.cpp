d6951615aec8da8fcc5032b73a0aa37af163b4dc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-01 00:56:19 +0200

    hidl: biometrics: fingerprint: Set properties for IFAA
    Change-Id: I0b1786721c951cd833d3c20f58cd0c8a70c08fae

--- hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
+++ hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
@@ -12,6 +12,7 @@
 #include <hardware/hardware.h>
 #include "BiometricsFingerprint.h"
 
+#include <android-base/properties.h>
 #include <inttypes.h>
 #include <unistd.h>
 
@@ -41,6 +42,8 @@ static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 
 using RequestStatus = android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
 
+using ::android::base::SetProperty;
+
 BiometricsFingerprint* BiometricsFingerprint::sInstance = nullptr;
 
 BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevice(nullptr) {
@@ -54,10 +57,16 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
 
         ALOGI("Opened fingerprint HAL, class %s", class_name);
         mIsUdfps = is_udfps;
+        SetProperty("persist.vendor.sys.fp.vendor", class_name);
         break;
     }
     if (!mDevice) {
         ALOGE("Can't open any HAL module");
+        SetProperty("persist.vendor.sys.fp.vendor", "none");
+    }
+
+    if (mIsUdfps) {
+        SetProperty("ro.hardware.fp.udfps", "true");
     }
 }
 
