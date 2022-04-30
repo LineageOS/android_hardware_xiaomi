db431b4d11af8216b7e3b6c93689693228ec8606
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-01 00:56:19 +0200

    hidl: biometrics: fingerprint: Set properties for IFAA
    Change-Id: I0b1786721c951cd833d3c20f58cd0c8a70c08fae

--- hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
+++ hidl/biometrics/fingerprint/BiometricsFingerprint.cpp
@@ -14,6 +14,7 @@
 #include "fingerprint.h"
 
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <fcntl.h>
 #include <inttypes.h>
 #include <poll.h>
@@ -70,6 +71,8 @@ static const fingerprint_hal_t kModules[] = {
 
 using RequestStatus = android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
 
+using ::android::base::SetProperty;
+
 BiometricsFingerprint* BiometricsFingerprint::sInstance = nullptr;
 
 BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevice(nullptr) {
@@ -83,6 +86,7 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
 
         LOG(INFO) << "Opened fingerprint HAL, class " << class_name;
         mIsUdfps = is_udfps;
+        SetProperty("persist.vendor.sys.fp.vendor", class_name);
         break;
     }
     if (!mDevice) {
@@ -121,6 +125,8 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
                                 readBool(fd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
             }
         }).detach();
+
+        SetProperty("ro.hardware.fp.fod", "true");
     }
 }
 
