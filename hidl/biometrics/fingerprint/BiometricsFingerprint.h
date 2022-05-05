285a312393ae19df8fc0d060b81aeeacc2961bef
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-05 15:42:55 +0200

    hidl: biometrics: fingerprint: Import AOSP fingerprint hardware header
    This will be customized with Xiaomi changes further down the line.
    
    Based off https://github.com/LineageOS/android_hardware_libhardware/commit/1ae7379baab4b2941ad1701190718cf53a051b48
    
    Change-Id: I9fff79d0052c09bc17fcd58489c828cb435c7498

--- hidl/biometrics/fingerprint/BiometricsFingerprint.h
+++ hidl/biometrics/fingerprint/BiometricsFingerprint.h
@@ -20,10 +20,10 @@
 #include <log/log.h>
 #include <android/log.h>
 #include <hardware/hardware.h>
-#include <hardware/fingerprint.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
+#include "fingerprint.h"
 
 namespace android {
 namespace hardware {
