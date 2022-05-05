3ab07b5f9f3ce33302cf4b8719c4fa2223c4fbee
Arian <arian.kulmer@web.de>
2022-05-05 23:30:30 +0200

    hidl: biometrics: fingerprint: Add support for device specific UDFPS libs
    Change-Id: I8f29d58b178aa8ff420204f790c6952fdf4ade58

---
+++ hidl/biometrics/fingerprint/include/UdfpsHandler.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "fingerprint.h"
+
+class UdfpsHandler {
+  public:
+    virtual ~UdfpsHandler() = default;
+
+    virtual void init(fingerprint_device_t *device) = 0;
+    virtual void onFingerDown(uint32_t x, uint32_t y, float minor, float major) = 0;
+    virtual void onFingerUp() = 0;
+};
+
+struct UdfpsHandlerFactory {
+    UdfpsHandler* (*create)();
+    void (*destroy)(UdfpsHandler* handler);
+};
+
+UdfpsHandlerFactory *getUdfpsHandlerFactory();
