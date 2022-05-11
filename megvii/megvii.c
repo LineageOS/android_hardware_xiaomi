01b01c97c5ae1361787035de42a40dbe5f25597c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-11 11:52:33 +0200

    Reland "megvii: Initial stub libraries"
    * We can't provide the original libs anymore, thus making the duplicated targets not an issue anymore
    
    This reverts commit c6a4249c37b6ca9dd984182a45bef998ac6912ae.
    
    Change-Id: Id987607b73ca0d8e9fd944518442f8cdffd0ce2a

---
+++ megvii/megvii.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+static int stub_fail() {
+    return -1;
+}
+
+void *mg_facepp[] = {
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+    &stub_fail,
+};
