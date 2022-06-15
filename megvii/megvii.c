e96f9b337c8ebb7213c6c63b4cfe7c22d0bfdf68
Tim Schumacher <timschumi@gmx.de>
2022-06-15 12:39:09 +0200

    megvii: Return zero for some stubs instead
    `mg_facepp[3]` and `mg_facepp[4]` seem to return version strings, which
    occasionally get logged if the return value is non-zero.
    
    Return a zero value for those functions to avoid crashing in strlen.
    
    Change-Id: Icc5d2ab2f72c480f03e652e3849aceaf4cfb498b

--- megvii/megvii.c
+++ megvii/megvii.c
@@ -8,12 +8,16 @@ static int stub_fail() {
     return -1;
 }
 
+static int stub_fail_zero() {
+    return 0;
+}
+
 void *mg_facepp[] = {
     &stub_fail,
     &stub_fail,
     &stub_fail,
-    &stub_fail,
-    &stub_fail,
+    &stub_fail_zero,
+    &stub_fail_zero,
     &stub_fail,
     &stub_fail,
     &stub_fail,
