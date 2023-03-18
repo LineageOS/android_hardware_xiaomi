ee03f414d616be3c66d2a4d24286f63d95bfb093
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-19 00:10:33 +0100

    doze: Initial version
    * Extracted from sdm845-common, converted to Kotlin, moved to AndroidX
      libraries where possible and addressed Android Studio warnings
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I0af5dcce7e637ac7327d96cdba32cbbc530bfd25

---
+++ doze/src/org/lineageos/settings/doze/DozeUtils.kt
@@ -0,0 +1,99 @@
+/*
+ * SPDX-FileCopyrightText: 2015 The CyanogenMod Project
+ * SPDX-FileCopyrightText: 2017-2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager.NameNotFoundException
+import android.hardware.display.AmbientDisplayConfiguration
+import android.os.UserHandle
+import android.provider.Settings
+import android.provider.Settings.Secure.DOZE_ALWAYS_ON
+import android.provider.Settings.Secure.DOZE_ENABLED
+import android.util.Log
+
+import androidx.preference.PreferenceManager
+
+object DozeUtils {
+    private const val LOG_TAG = "DozeUtils"
+
+    internal const val DOZE_ENABLE = "doze_enable"
+
+    internal const val ALWAYS_ON_DISPLAY = "always_on_display"
+
+    internal const val CATEG_PICKUP_SENSOR = "pickup_sensor"
+    internal const val CATEG_PROX_SENSOR = "proximity_sensor"
+
+    internal const val GESTURE_PICK_UP_KEY = "gesture_pick_up"
+    internal const val GESTURE_HAND_WAVE_KEY = "gesture_hand_wave"
+    internal const val GESTURE_POCKET_KEY = "gesture_pocket"
+
+    private const val DOZE_INTENT = "com.android.systemui.doze.pulse"
+
+    internal fun getProxCheckBeforePulse(context: Context) = try {
+        val con = context.createPackageContext("com.android.systemui", 0)
+        val id = con.resources.getIdentifier(
+            "doze_proximity_check_before_pulse",
+            "bool", "com.android.systemui"
+        )
+        con.resources.getBoolean(id)
+    } catch (e: NameNotFoundException) {
+        false
+    }
+
+    internal fun enableDoze(context: Context, enable: Boolean) = Settings.Secure.putInt(
+        context.contentResolver,
+        DOZE_ENABLED, if (enable) 1 else 0
+    )
+
+    fun isDozeEnabled(context: Context) = Settings.Secure.getInt(
+        context.contentResolver,
+        DOZE_ENABLED, 1
+    ) != 0
+
+    fun launchDozePulse(context: Context) {
+        Log.d(LOG_TAG, "Launch doze pulse")
+
+        context.sendBroadcastAsUser(
+            Intent(DOZE_INTENT),
+            UserHandle(UserHandle.USER_CURRENT)
+        )
+    }
+
+    internal fun enableAlwaysOn(context: Context, enable: Boolean) =
+        Settings.Secure.putIntForUser(
+            context.contentResolver,
+            DOZE_ALWAYS_ON, if (enable) 1 else 0, UserHandle.USER_CURRENT
+        )
+
+    internal fun isAlwaysOnEnabled(context: Context): Boolean {
+        val enabledByDefault = context.resources
+            .getBoolean(com.android.internal.R.bool.config_dozeAlwaysOnEnabled)
+        return Settings.Secure.getIntForUser(
+            context.contentResolver,
+            DOZE_ALWAYS_ON, if (alwaysOnDisplayAvailable(context) && enabledByDefault) 1 else 0,
+            UserHandle.USER_CURRENT
+        ) != 0
+    }
+
+    internal fun alwaysOnDisplayAvailable(context: Context) =
+        AmbientDisplayConfiguration(context).alwaysOnAvailable()
+
+    private fun isGestureEnabled(context: Context, gesture: String?) =
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(gesture, false)
+
+    fun isPickUpEnabled(context: Context) = isGestureEnabled(context, GESTURE_PICK_UP_KEY)
+    fun isHandwaveGestureEnabled(context: Context) =
+        isGestureEnabled(context, GESTURE_HAND_WAVE_KEY)
+    fun isPocketGestureEnabled(context: Context) = isGestureEnabled(context, GESTURE_POCKET_KEY)
+
+    fun sensorsEnabled(context: Context) =
+        (isPickUpEnabled(context)
+                || isHandwaveGestureEnabled(context)
+                || isPocketGestureEnabled(context))
+}
---
+++ doze/src/org/lineageos/settings/doze/ProximitySensor.kt
@@ -0,0 +1,91 @@
+/*
+ * SPDX-FileCopyrightText: 2015 The CyanogenMod Project
+ * SPDX-FileCopyrightText: 2017-2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+
+import org.lineageos.settings.doze.DozeUtils.isHandwaveGestureEnabled
+import org.lineageos.settings.doze.DozeUtils.isPocketGestureEnabled
+import org.lineageos.settings.doze.DozeUtils.launchDozePulse
+
+import java.util.concurrent.Executors
+
+class ProximitySensor(private val context: Context) : SensorEventListener {
+    var sawNear = false
+        private set
+    private val sensorManager = context.getSystemService(SensorManager::class.java)
+    private val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false)
+    private val executorService = Executors.newSingleThreadExecutor()
+    private var inPocketTime = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (DEBUG) Log.d(LOG_TAG, "Got sensor event: ${event.values[0]}")
+
+        val isNear = event.values[0] < sensor.maximumRange
+
+        if (sawNear && !isNear) {
+            if (shouldPulse(event.timestamp)) {
+                launchDozePulse(context)
+            }
+        } else {
+            inPocketTime = event.timestamp
+        }
+
+        sawNear = isNear
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    fun enable() {
+        Log.d(LOG_TAG, "Enabling")
+
+        submit {
+            sensorManager.registerListener(
+                this, sensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    fun disable() {
+        Log.d(LOG_TAG, "Disabling")
+
+        submit { sensorManager.unregisterListener(this, sensor) }
+    }
+
+    private fun submit(runnable: Runnable) = executorService.submit(runnable)
+
+    private fun shouldPulse(timestamp: Long): Boolean {
+        val delta = timestamp - inPocketTime
+
+        return if (isHandwaveGestureEnabled(context) && isPocketGestureEnabled(context)) {
+            true
+        } else if (isHandwaveGestureEnabled(context)) {
+            delta < HANDWAVE_MAX_DELTA_NS
+        } else if (isPocketGestureEnabled(context)) {
+            delta >= POCKET_MIN_DELTA_NS
+        } else {
+            false
+        }
+    }
+
+    companion object {
+        private const val LOG_TAG = "ProximitySensor"
+        private const val DEBUG = false
+
+        // Maximum time for the hand to cover the sensor: 1s
+        private const val HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000
+
+        // Minimum time until the device is considered to have been in the pocket: 2s
+        private const val POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000
+    }
+}
