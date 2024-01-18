c8a56c3feae2cdd8cc034ce9fcfcdb2036c0dfa5
someone5678 <someone5678@users.noreply.github.com>
2024-01-18 20:17:28 +0900

    Euicc: Don't fatally crash when euicc package is not exists
    Log:
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: FATAL EXCEPTION: main
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: Process: org.lineageos.euicc, PID: 5014
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: java.lang.RuntimeException: Unable to start receiver org.lineageos.euicc.BootCompletedReceiver: java.lang.IllegalArgumentException: Unknown package: com.google.euiccpixel
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4500)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleReceiver(Unknown Source:0)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2282)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8291)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:978)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: Caused by: java.lang.IllegalArgumentException: Unknown package: com.google.euiccpixel
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Parcel.createExceptionOrNull(Parcel.java:3061)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Parcel.createException(Parcel.java:3041)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3024)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:2966)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.content.pm.IPackageManager$Stub$Proxy.setApplicationEnabledSetting(IPackageManager.java:6107)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ApplicationPackageManager.setApplicationEnabledSetting(ApplicationPackageManager.java:3185)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at org.lineageos.euicc.EuiccDisabler.enableOrDisableEuicc(EuiccDisabler.kt:43)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at org.lineageos.euicc.BootCompletedReceiver.onReceive(BootCompletedReceiver.kt:16)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4491)
    01-18 20:01:59.274  5014  5014 E AndroidRuntime: 	... 9 more
    
    Change-Id: I86880342693c92afddf09dbe8db22255ca93c355

--- Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
+++ Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
@@ -6,6 +6,7 @@
 package org.lineageos.euicc
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PackageInfoFlags
 import android.util.Log
@@ -32,6 +33,11 @@ object EuiccDisabler {
         "com.google.android.euicc",
     )
 
+    private fun isInstalled(pm: PackageManager, pkgName: String) = runCatching {
+        val info = pm.getPackageInfo(pkgName, PackageInfoFlags.of(0))
+        info.applicationInfo.flags and ApplicationInfo.FLAG_INSTALLED != 0
+    }.getOrDefault(false)
+
     private fun isInstalledAndEnabled(pm: PackageManager, pkgName: String) = runCatching {
         val info = pm.getPackageInfo(pkgName, PackageInfoFlags.of(0))
         Log.d(TAG, "package $pkgName installed, enabled = ${info.applicationInfo.enabled}")
@@ -54,7 +60,9 @@ object EuiccDisabler {
         }
 
         for (pkg in EUICC_PACKAGES) {
-            pm.setApplicationEnabledSetting(pkg, flag, 0)
+            if (isInstalled(pm, pkg)) {
+                pm.setApplicationEnabledSetting(pkg, flag, 0)
+            }
         }
     }
 }
