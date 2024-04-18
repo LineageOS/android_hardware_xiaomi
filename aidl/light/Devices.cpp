72c42acaaf40f621c62e5b3e043da24803771fe1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-04-18 17:34:39 +0200

    aidl: light: Refactor
    Change-Id: Ib4ae8e9821dd0fb36e922e2c5f993aa6c0e28be1

---
+++ aidl/light/Devices.cpp
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Devices.h"
+
+#define LOG_TAG "Devices"
+
+#include <android-base/logging.h>
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace light {
+
+static const std::string kBacklightDevices[] = {
+        "backlight",
+        "panel0-backlight",
+};
+
+static std::vector<BacklightDevice> getBacklightDevices() {
+    std::vector<BacklightDevice> devices;
+
+    for (const auto& device : kBacklightDevices) {
+        auto backlight = BacklightDevice(device);
+        if (backlight.exists()) {
+            LOG(INFO) << "Found backlight device " << device;
+            devices.push_back(backlight);
+        }
+    }
+
+    return devices;
+}
+
+static const std::string kLedBacklightDevices[] = {
+        "lcd-backlight",
+};
+
+static std::vector<LedDevice> getBacklightLedDevices() {
+    std::vector<LedDevice> devices;
+
+    for (const auto& device : kLedBacklightDevices) {
+        auto backlight = LedDevice(device);
+        if (backlight.exists()) {
+            LOG(INFO) << "Found LED backlight device " << device;
+            devices.push_back(backlight);
+        }
+    }
+
+    return devices;
+}
+
+static const std::string kButtonLedDevices[] = {
+        "button-backlight",
+        "button-backlight1",
+};
+
+static std::vector<LedDevice> getButtonLedDevices() {
+    std::vector<LedDevice> devices;
+
+    for (const auto& device : kButtonLedDevices) {
+        auto button = LedDevice(device);
+        if (button.exists()) {
+            LOG(INFO) << "Found button device " << device;
+            devices.emplace_back(button);
+        }
+    }
+
+    return devices;
+}
+
+static RgbLedDevice getRgbLedDevice() {
+    auto red = LedDevice("red");
+    auto green = LedDevice("green");
+    auto blue = LedDevice("blue");
+
+    return RgbLedDevice(red, green, blue);
+}
+
+static LedDevice getWhiteLedDevice() {
+    return LedDevice("white");
+}
+
+Devices::Devices()
+    : mBacklightDevices(getBacklightDevices()),
+      mBacklightLedDevices(getBacklightLedDevices()),
+      mButtonLedDevices(getButtonLedDevices()),
+      mRgbLedDevice(getRgbLedDevice()),
+      mWhiteLed(getWhiteLedDevice()) {
+    if (!hasBacklightDevices()) {
+        LOG(INFO) << "No backlight devices found";
+    }
+
+    if (!hasButtonDevices()) {
+        LOG(INFO) << "No button devices found";
+    }
+
+    if (!hasNotificationDevices()) {
+        LOG(INFO) << "No notification devices found";
+    }
+}
+
+bool Devices::hasBacklightDevices() {
+    return !mBacklightDevices.empty() || !mBacklightLedDevices.empty();
+}
+
+bool Devices::hasButtonDevices() {
+    return !mButtonLedDevices.empty();
+}
+
+bool Devices::hasNotificationDevices() {
+    return mRgbLedDevice.exists() || mWhiteLed.exists();
+}
+
+void Devices::setBacklightColor(rgb color) {
+    for (auto& device : mBacklightDevices) {
+        device.setBrightness(color.toBrightness());
+    }
+    for (auto& device : mBacklightLedDevices) {
+        device.setBrightness(color.toBrightness());
+    }
+}
+
+void Devices::setButtonsColor(rgb color) {
+    for (auto& device : mButtonLedDevices) {
+        device.setBrightness(color.toBrightness());
+    }
+}
+
+void Devices::setNotificationColor(rgb color, LightMode mode) {
+    bool rc = true;
+
+    switch (mode) {
+        case LightMode::BREATH:
+            if (mWhiteLed.exists()) {
+                rc &= mWhiteLed.setBrightness(color.toBrightness(), LightMode::BREATH);
+            } else {
+                rc &= mRgbLedDevice.setBrightness(color, LightMode::BREATH);
+            }
+
+            if (rc) {
+                break;
+            }
+
+            // Fallback to static mode if breath is not supported
+            rc = true;
+
+            FALLTHROUGH_INTENDED;
+        case LightMode::STATIC:
+            if (mWhiteLed.exists()) {
+                rc &= mWhiteLed.setBrightness(color.toBrightness(), LightMode::STATIC);
+            } else {
+                rc &= mRgbLedDevice.setBrightness(color, LightMode::STATIC);
+            }
+            break;
+        default:
+            LOG(ERROR) << "Unsupported light mode " << mode;
+            break;
+    }
+}
+
+}  // namespace light
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
--- aidl/light/Lights.h
+++ aidl/light/Lights.h
@@ -8,7 +8,7 @@
 
 #include <aidl/android/hardware/light/BnLights.h>
 #include <mutex>
-#include "Backlight.h"
+#include "Devices.h"
 
 using ::aidl::android::hardware::light::HwLight;
 using ::aidl::android::hardware::light::HwLightState;
@@ -26,19 +26,16 @@ class Lights : public BnLights {
     ndk::ScopedAStatus getLights(std::vector<HwLight>* _aidl_return) override;
 
   private:
-    void setLED();
-
     std::vector<HwLight> mLights;
 
-    BacklightDevice* mBacklightDevice;
-    std::vector<std::string> mButtonsPaths;
-    bool mWhiteLED;
-
-    std::mutex mLEDMutex;
+    Devices mDevices;
 
     HwLightState mLastBatteryState;
-    HwLightState mLastNotificationState;
+    HwLightState mLastNotificationsState;
     HwLightState mLastAttentionState;
+    std::mutex mLedMutex;
+
+    void updateNotificationColor();
 };
 
 }  // namespace light
