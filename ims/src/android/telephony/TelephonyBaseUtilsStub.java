30ad2815760e498cbfb1089a9b12bc0d6d80db22
danielml <daniel@danielml.dev>
2024-06-16 10:30:40 +0200

    Provide a Java library with required Xiaomi IMS methods
    To make this jar available to all system APKs, load it as bootjar:
    
    ```
    PRODUCT_PACKAGES += \
        ims-support
    
    PRODUCT_BOOT_JARS += \
        ims-support
    ```
    
    Change-Id: I69006732e6f915dc2a5cd1a3262b7ccf37372986

---
+++ ims/src/android/telephony/TelephonyBaseUtilsStub.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package android.telephony;
+
+public class TelephonyBaseUtilsStub {
+    public static boolean isMiuiRom() {
+        return false;
+    }
+}
