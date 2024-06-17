7329fd774f362d456ed0fbc0ff9215304669fb04
Bruno Martins <bgcngm@gmail.com>
2024-06-18 01:45:59 +0200

    aidl: light: Add support for timed mode
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Change-Id: I23623c7527cf98230f890935a4742f86adc3b3ea

--- aidl/light/LedDevice.cpp
+++ aidl/light/LedDevice.cpp
@@ -29,6 +29,16 @@ static const std::string kBreathNodes[] = {
         "blink",
 };
 
+static const std::string kBlinkNode = "blink";
+static const std::string kStartIdxNode = "start_idx";
+static const std::string kDutyPctsNode = "duty_pcts";
+static const std::string kPauseLoNode = "pause_lo";
+static const std::string kPauseHiNode = "pause_hi";
+static const std::string kRampStepMsNode = "ramp_step_ms";
+
+static constexpr int kRampSteps = 8;
+static constexpr int kRampMaxStepDurationMs = 50;
+
 LedDevice::LedDevice(std::string name) : mName(name), mBasePath(kBaseLedsPath + name + "/") {
     if (!readFromFile(mBasePath + kMaxBrightnessNode, mMaxBrightness)) {
         mMaxBrightness = kDefaultMaxBrightness;
@@ -40,6 +50,13 @@ LedDevice::LedDevice(std::string name) : mName(name), mBasePath(kBaseLedsPath + 
             break;
         }
     }
+
+    mSupportsTimed = std::ifstream(mBasePath + kBlinkNode).good() &&
+                     std::ifstream(mBasePath + kStartIdxNode).good() &&
+                     std::ifstream(mBasePath + kDutyPctsNode).good() &&
+                     std::ifstream(mBasePath + kPauseLoNode).good() &&
+                     std::ifstream(mBasePath + kPauseHiNode).good() &&
+                     std::ifstream(mBasePath + kRampStepMsNode).good();
 }
 
 std::string LedDevice::getName() const {
@@ -50,17 +67,57 @@ bool LedDevice::supportsBreath() const {
     return !mBreathNode.empty();
 }
 
+bool LedDevice::supportsTimed() const {
+    return mSupportsTimed;
+}
+
 bool LedDevice::exists() const {
     return std::ifstream(mBasePath + kBrightnessNode).good();
 }
 
-bool LedDevice::setBrightness(uint8_t value, LightMode mode) {
+static std::string getScaledDutyPercent(uint8_t brightness) {
+    std::string output;
+    for (int i = 0; i <= kRampSteps; i++) {
+        if (i != 0) {
+            output += ",";
+        }
+        output += std::to_string(i * 100 * brightness / (0xFF * kRampSteps));
+    }
+    return output;
+}
+
+bool LedDevice::setBrightness(uint8_t value, LightMode mode, uint32_t flashOnMs,
+                              uint32_t flashOffMs) {
     // Disable current blinking
+    if (mSupportsTimed) {
+        writeToFile(mBasePath + kBlinkNode, 0);
+    }
     if (supportsBreath()) {
         writeToFile(mBasePath + mBreathNode, 0);
     }
 
     switch (mode) {
+        case LightMode::TIMED:
+            if (mSupportsTimed) {
+                int32_t stepDuration = kRampMaxStepDurationMs;
+                int32_t pauseLo = flashOffMs;
+                int32_t pauseHi = flashOnMs - (stepDuration * kRampSteps * 2);
+
+                if (pauseHi < 0) {
+                    stepDuration = flashOnMs / (kRampSteps * 2);
+                    pauseHi = 0;
+                }
+
+                return writeToFile(mBasePath + kStartIdxNode, 0) &&
+                       writeToFile(mBasePath + kDutyPctsNode, getScaledDutyPercent(value)) &&
+                       writeToFile(mBasePath + kPauseLoNode, pauseLo) &&
+                       writeToFile(mBasePath + kPauseHiNode, pauseHi) &&
+                       writeToFile(mBasePath + kRampStepMsNode, stepDuration) &&
+                       writeToFile(mBasePath + kBlinkNode, 1);
+            }
+
+            // Fallthrough to breath mode if timed is not supported
+            FALLTHROUGH_INTENDED;
         case LightMode::BREATH:
             if (supportsBreath()) {
                 return writeToFile(mBasePath + mBreathNode, value > 0 ? 1 : 0);
@@ -85,6 +142,7 @@ void LedDevice::dump(int fd) const {
     dprintf(fd, ", base path: %s", mBasePath.c_str());
     dprintf(fd, ", max brightness: %u", mMaxBrightness);
     dprintf(fd, ", supports breath: %d", supportsBreath());
+    dprintf(fd, ", supports timed: %d", supportsTimed());
     dprintf(fd, ", breath node: %s", mBreathNode.c_str());
 }
 
