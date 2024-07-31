043422914e49191700a602d75f341cd8366b32c1
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-31 16:58:44 +0530

    Euicc: Reverse the sku check
    Change-Id: I456f01fbd1de41402ff4142fb497b3e49dfc3112

--- Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
+++ Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
@@ -41,9 +41,9 @@ object EuiccDisabler {
     fun enableOrDisableEuicc(context: Context) {
         val pm = context.packageManager
         val sku = SystemProperties.get("ro.boot.product.hardware.sku")
-        val disable = if (sku != "GL") {
-            Log.d(TAG, "Disabling apps due to non-GL SKU")
-            true // Disable if SKU is not GL
+        val disable = if (sku == "IN" || sku == "CN") {
+            Log.d(TAG, "Disabling apps due to IN or CN SKU")
+            true // Disable if SKU is IN or CN
         } else {
             EUICC_DEPENDENCIES.any { !isInstalledAndEnabled(pm, it) }
         }
