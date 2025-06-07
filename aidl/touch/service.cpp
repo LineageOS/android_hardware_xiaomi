cc0023e431124b41c69dc477905139c17f831929
Bruno Martins <bgcngm@gmail.com>
2025-06-07 14:46:40 +0100

    touch: Include KeyDisabler and KeySwapper generic support
    This adds conditional support for KeyDisabler and KeySwapper
    along with HighTouchPollingRate feature.
    
    Change-Id: Ie6c0e7c1032e9d44bf03e6f6738581a4418fb645

--- aidl/touch/service.cpp
+++ aidl/touch/service.cpp
@@ -3,21 +3,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define LOG_TAG "vendor.lineage.touch-service.xiaomi"
+
 #include "HighTouchPollingRate.h"
+#include "KeyDisabler.h"
+#include "KeySwapper.h"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 
 using aidl::vendor::lineage::touch::HighTouchPollingRate;
+using aidl::vendor::lineage::touch::KeyDisabler;
+using aidl::vendor::lineage::touch::KeySwapper;
 
 int main() {
-    ABinderProcess_setThreadPoolMaxThreadCount(0);
-    std::shared_ptr<HighTouchPollingRate> htpr = ndk::SharedRefBase::make<HighTouchPollingRate>();
+    binder_status_t status = STATUS_OK;
 
-    const std::string instance = std::string(HighTouchPollingRate::descriptor) + "/default";
-    binder_status_t status = AServiceManager_addService(htpr->asBinder().get(), instance.c_str());
-    CHECK_EQ(status, STATUS_OK);
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+
+#ifdef HTPR_CONTROL_PATH
+    std::shared_ptr<HighTouchPollingRate> htpr = ndk::SharedRefBase::make<HighTouchPollingRate>();
+    const std::string htpr_instance = std::string(HighTouchPollingRate::descriptor) + "/default";
+    status = AServiceManager_addService(htpr->asBinder().get(), htpr_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << htpr_instance << " " << status;
+#endif
+
+#ifdef KD_CONTROL_PATH
+    std::shared_ptr<KeyDisabler> kd = ndk::SharedRefBase::make<KeyDisabler>();
+    const std::string kd_instance = std::string(KeyDisabler::descriptor) + "/default";
+    status = AServiceManager_addService(kd->asBinder().get(), kd_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << kd_instance << " " << status;
+#endif
+
+#ifdef KS_CONTROL_PATH
+    std::shared_ptr<KeySwapper> ks = ndk::SharedRefBase::make<KeySwapper>();
+    const std::string ks_instance = std::string(KeySwapper::descriptor) + "/default";
+    status = AServiceManager_addService(ks->asBinder().get(), ks_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << ks_instance << " " << status;
+#endif
 
     ABinderProcess_joinThreadPool();
     return EXIT_FAILURE;  // should not reach
