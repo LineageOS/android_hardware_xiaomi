894ee2d2ef4cf7218cea30a934d9a4f75c0e8d44
Ido <dev.xyzuniverse@gmail.com>
2025-08-18 03:35:46 +0200

    aidl: fingerprint: Add jiiov module
    Change-Id: I5d550d361d3dc746b67b7ebc307cefb80287908d
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- aidl/fingerprint/Fingerprint.cpp
+++ aidl/fingerprint/Fingerprint.cpp
@@ -34,7 +34,7 @@ typedef struct fingerprint_hal {
 
 static const fingerprint_hal_t kModules[] = {
         {"fortsense"},  {"fpc"},         {"fpc_fod"}, {"goodix"}, {"goodix:gf_fingerprint"},
-        {"goodix_fod"}, {"goodix_fod6"}, {"silead"},  {"syna"},
+        {"goodix_fod"}, {"goodix_fod6"}, {"silead"},  {"syna"}, {"jiiov"},
 };
 
 }  // namespace
