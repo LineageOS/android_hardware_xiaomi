a215313a6aff18e7bbabee862ef2fc8c534d2db6
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-25 13:29:46 +0530

    xiaomi: Add initial sepolicy includes
    Change-Id: I1f6190ae5bc6765859b0df4b0f81d0a1aafaf239

---
+++ sepolicy/qti/SEPolicy.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/xiaomi/sepolicy/qti/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/xiaomi/sepolicy/qti/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/xiaomi/sepolicy/qti/public
